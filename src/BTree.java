a3ccf2e1adb8956608b06a21e790f44d23537be4
mohammed <mohammedmasri1@gmail.com>
2018-05-20 18:56:02 +0300

    new BTree data type, the AVL tree is still broken.

--- src/BTree.java
+++ src/BTree.java
@@ -4,17 +4,30 @@ public class BTree{
     private BTreeNode root;
     private int order;
     public BTree(int r,int order) {
-        root = new BTreeNode(r);
+        root = new BTreeNode(order);
         this.order = order;
         if(order<2){throw new IllegalArgumentException("order must be greater than or equal to 2");}
     }
     public void insert(int t){
 
     }
-    private class BTreeNode implements Comparable<BTreeNode>{
+    private class BTreeNode{
+        Node[] list;
+        int size = 0;
+        boolean leaf = true;
+        public BTreeNode(int order) {
+            list = new Node[order];
+        }
+        void insert(int t){
+            list[size] = new Node(t);
+            Arrays.sort(list);
+            size++;
+        }
+    }
+    private class Node implements Comparable<Node>{
         private int[] left,right;
         private int element;
-        public BTreeNode(int element) {
+        public Node(int element) {
             this.element = element;
         }
         public int[] getLeft() {
@@ -30,7 +43,7 @@ public class BTree{
         }
 
         @Override
-        public int compareTo(BTreeNode o) {
+        public int compareTo(Node o) {
             return element-o.element;
         }
     }
